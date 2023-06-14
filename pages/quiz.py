7c32b04beba7498fe8ce9e8dfed078aaf5362d12
ayshmnmm <65579576+ayshmnmm@users.noreply.github.com>
2023-06-14 06:06:39 +0530

    quiz - add basic working page

--- pages/quiz.py
+++ pages/quiz.py
@@ -1,6 +1,7 @@
 import tkinter
 from pages import settings, menu
 import os
+import random
 
 
 class Page:
@@ -18,7 +19,7 @@ class Page:
         self.title =  tkinter.PhotoImage(file=os.path.join(self.cwd,'img/quiz.png'))
         tkinter.Button(self.top_bar, image=self.back_btn, borderwidth=0, background='white', command=self.close).pack(side='left', padx=30, pady=25)
         tkinter.Label(self.top_bar, image=self.title, borderwidth=0).place(anchor='center', x = settings.WIDTH//2, y = 50)
-        Game(self.page, self.cwd, self)
+        Game(self.page, self.cwd, self, self.top_bar)
 
 
     def close(self):
@@ -28,27 +29,77 @@ class Page:
 
 
 class Game(Page):
-    def __init__(self, parent, cwd, ref):
-        self.xo_board = [[' ','X','O'],[' ',' ',' '],[' ',' ',' ']]
+    def __init__(self, parent, cwd, ref, top_bar):
+        self.questions = settings.QUIZ_QUESTIONS.copy()
+        self.score = 0
         self.parent = parent
         self.cwd = cwd
         self.ref = ref
-        self.turn = 1
-        self.area = tkinter.Frame(self.parent, background='green')
-        self.area.pack(expand=True, fill='both', padx=150, pady=75)
+        self.count = 0
+        self.top_bar = top_bar
+        self.quiz_area = tkinter.Frame(self.parent, background='white')
+        self.quiz_area.pack(expand=True, fill='both')
+        self.scoreboard = tkinter.Label(self.top_bar, text=f'', font=('Arial', 25), background='white')
+        self.scoreboard.pack(side='right', padx=15)
+        
+        ques = random.choice(self.questions)
+        self.questions.remove(ques)
+        self.display(ques[0],ques[1],ques[2])
+        
+
+    def display(self, question, options, correct_index):
+        self.area = tkinter.Frame(self.quiz_area, background='white')
+        self.area.pack(expand=True, fill='both', padx=120, pady=75)
         self.area.rowconfigure(0,weight=1)
         self.area.rowconfigure(1,weight=1)
         self.area.rowconfigure(2,weight=1)
         self.area.rowconfigure(3,weight=1)
+        self.area.rowconfigure(4,weight=1)
         self.area.columnconfigure(0,weight=1)
+        tkinter.Label(self.area, text=question, font=('Arial',15), background='white', wraplength=360).grid(row=0, column=0, sticky='n')
+        self.buttons = []
+        for i in range(1,5):
+            self.buttons.append(tkinter.Button(self.area, text=options[i-1], background='white', highlightcolor='salmon', command=lambda x=i-1, bts=self.buttons:self.click(x,correct_index,bts), relief=tkinter.GROOVE, borderwidth=1))
+        
+        randorder = list(range(4))
+        random.shuffle(randorder)
+        k = 0
+        for i in randorder:
+            self.buttons[k].grid(row=i+1, column=0, sticky='ewns', pady=2)
+            k += 1
 
-        self.ref.ximg =  tkinter.PhotoImage(file=os.path.join(self.cwd,'img/X.png'))
-        self.ref.oimg =  tkinter.PhotoImage(file=os.path.join(self.cwd,'img/O.png'))
-        self.ref.bimg =  tkinter.PhotoImage(file=os.path.join(self.cwd,'img/blank.png'))
-        self.display()
+    def ask_questions(self):
+        self.area.destroy()
+        ques = random.choice(self.questions)
+        self.questions.remove(ques)
+        self.display(ques[0],ques[1],ques[2])
+        if self.count >= settings.QUESTIONS_COUNT: 
+            self.gameover()
 
-    def display(self):
-        for i in range(4):
-            tkinter.Button(self.area, text="replace soon").grid(row=i, column=0, sticky='ewns')
+    def click(self, choice, correct_index, buttons):
+        for button in buttons:
+            button.config(state=tkinter.DISABLED)
+        self.count += 1
+        if choice==correct_index:
+            buttons[choice].config(background='lime')
+            self.score += 1
+        else:
+            buttons[choice].config(background='salmon')
+        self.scoreboard.config(text=f'{self.score}/{self.count}')
+        self.area.after(1000,lambda:self.ask_questions())
 
+    def gameover(self):
+        self.area.destroy()
+        self.message = tkinter.Frame(self.quiz_area, background='white')
+        self.message.pack(expand=True,fill='both', padx=120, pady=75)
+        self.message.rowconfigure(0,weight=1)
+        self.message.rowconfigure(0,weight=1)
+        self.message.columnconfigure(0,weight=1)
+        tkinter.Label(self.message, text=f'{(100*self.score/self.count):.2f}%', font=('Arial',50), background='white').grid(row=0,column=0)
+        tkinter.Button(self.message, text='Play Again', background='white', font=('Arial',20), command=self.replay, relief=tkinter.GROOVE).grid(row=1,column=0, sticky='ew', pady=30, padx=40)
+        # tkinter.Label(self.message, text='OVERRR')
+
+    def replay(self):
+        Page(self.ref.parent,self.cwd)
+        self.parent.destroy()
 
--- pages/settings.py
+++ pages/settings.py
@@ -1,2 +1,31 @@
 WIDTH = 600
 HEIGHT = 600
+
+# quiz.py
+QUESTIONS_COUNT = 7
+QUIZ_QUESTIONS = [
+["What is the capital of Australia?", ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'], 2],
+["Who painted the Mona Lisa?", ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Michelangelo'], 0],
+["Which planet is known as the Red Planet?", ['Mars', 'Jupiter', 'Venus', 'Saturn'], 0],
+["What is the chemical symbol for gold?", ['Ag', 'Au', 'Hg', 'Pt'], 1],
+["Which country is known as the Land of the Rising Sun?", ['China', 'South Korea', 'Japan', 'Thailand'], 2],
+["What is the largest organ in the human body?", ['Liver', 'Brain', 'Skin', 'Heart'], 2],
+["Who wrote the play Romeo and Juliet?", ['William Shakespeare', 'Arthur Miller', 'Tennessee Williams', 'Anton Chekhov'], 0],
+["What is the tallest mountain in the world?", ['Mount Kilimanjaro', 'Mount Everest', 'Mount McKinley', 'Mount Fuji'], 1],
+["Which animal is known for its black and white stripes?", ['Giraffe', 'Tiger', 'Zebra', 'Penguin'], 2],
+["In which year did World War II end?", ['1943', '1945', '1947', '1950'], 1],
+["What is the smallest country in the world?", ['Vatican City', 'Monaco', 'Nauru', 'San Marino'], 0],
+["Which scientist developed the theory of general relativity?", ['Isaac Newton', 'Albert Einstein', 'Stephen Hawking', 'Galileo Galilei'], 1],
+["What is the national flower of India?", ['Lotus', 'Rose', 'Tulip', 'Sunflower'], 0],
+["Which composer wrote the Symphony No. 9 in D minor, also known as the 'Choral' Symphony?", ['Ludwig van Beethoven', 'Wolfgang Amadeus Mozart', 'Johann Sebastian Bach', 'Franz Schubert'], 0],
+["Who discovered penicillin?", ['Alexander Fleming', 'Marie Curie', 'Albert Einstein', 'Louis Pasteur'], 0],
+["What is the chemical formula for water?", ['H2O', 'CO2', 'NaCl', 'O2'], 0],
+["Which novel is set in the fictional Maycomb County?", ['Pride and Prejudice', 'To Kill a Mockingbird', '1984', 'The Great Gatsby'], 1],
+["What is the largest ocean on Earth?", ['Pacific Ocean', 'Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean'], 0],
+["Who wrote the novel 'War and Peace'?", ['Fyodor Dostoevsky', 'Leo Tolstoy', 'Virginia Woolf', 'Jane Austen'], 1],
+["What is the chemical symbol for iron?", ['Fe', 'Ir', 'In', 'Io'], 0],
+["Which artist painted the famous artwork 'The Starry Night'?", ['Pablo Picasso', 'Vincent van Gogh', 'Salvador Dalí', 'Claude Monet'], 1],
+["How many days are there in a leap year?", ['365', '366', '364', '363'], 1],
+["What is the capital of France?", ['Paris', 'Rome', 'London', 'Madrid'], 0],
+["I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?", ['Echo', 'Whistle', 'Music', 'Wave'], 0]
+]
